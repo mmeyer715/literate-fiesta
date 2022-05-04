7ded4a95085a9990d0b609cdf7beac343c181e8b
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-05-04 17:06:24 -0500

    Finish Category Routes

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ Category.init(
       autoIncrement: true
     },
     category_name: {
-      types: DataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false
     }
   },
--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { FOREIGNKEYS } = require('sequelize/types/lib/query-types');
+//const { FOREIGNKEYS } = require('sequelize/types/lib/query-types');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 const Category = require('./Category');
