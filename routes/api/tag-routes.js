38f0b42750d84eec01b4a6947abd5e44780c3f74
Maria Meyer <mbean1216@icloud.com>
2022-05-05 16:01:36 -0500

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   // be sure to include its associated Product data
   try{
     const tagData = await Tag.findAll({
-      include: [{model: ProductTag}]
+      include: [{model: ProductTag, Product}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
   try{
     const tagData = await Tag.findByPk(req.params.id,{
-      include: [{model: ProductTag}]
+      include: [{model: ProductTag, Product}]
     });
 
     if(!tagData){
