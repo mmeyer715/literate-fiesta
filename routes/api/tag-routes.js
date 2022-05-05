36242cd5d8460a702f257bffcc6453e769ac68fb
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-05-05 15:39:03 -0500

    Begin tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,45 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findAll({
+      include: [{model: ProductTag}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tagData = await Tag.findByPk(req.params.id,{
+      include: [{model: ProductTag}]
+    });
+
+    if(!tagData){
+      res.status(404).json({message: 'No Tag found with that id!'});
+      return;
+    }
+    
+    res.status(200).json(tagData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try{
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(tagData);
+  }catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
